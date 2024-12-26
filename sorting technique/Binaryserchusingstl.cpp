bea10645821c28e259cfb16e79884dba1546d0e8
saketmishra14 <mishrasaket042@gmail.com>
2024-12-26 18:00:24 +0530

    STL concept

---
+++ sorting technique/Binaryserchusingstl.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<algorithm> //for binary search 
+#include<vector>
+using namespace std;
+
+int main(){
+    vector<int>arr={2,3,4,5,7,8,9,10 };
+    int target;
+    cout<<"give the target:";
+    cin>>target;
+     if(binary_search(arr.begin(),arr.end(),target) ){
+        cout<<"Element found";
+     }
+     else{
+        cout<<"Not found.";
+     }
+
+}
