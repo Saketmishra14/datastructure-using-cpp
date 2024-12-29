231336297c824de4991f00e8301697e5be479305
saketmishra14 <mishrasaket042@gmail.com>
2024-12-29 11:53:51 +0530

    2d-array

---
+++ sorting technique/2-darrayusingbinarysearch.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+bool binarysearch(int arr[5][4],int row,int col,int target){
+
+}
+int main(){
+    int arr[5][4]={{1,2,3,4},{5,6,7,8},{9,10,11,12},{13,14,15,16},{17,18,19,20}};
+    int row=5;
+    int col=4;
+    int target=15;
+
+    bool ans=binarysearch(arr,row,col,target);
+     
+
+
+    return 0;
+}
