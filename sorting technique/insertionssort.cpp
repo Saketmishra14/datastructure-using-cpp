f7aa8b73cfaea3bb6971cf1a6a8c7afcb55eef12
saketmishra14 <mishrasaket042@gmail.com>
2024-12-30 23:19:08 +0530

    insertion sort

--- sorting technique/insertionssort.cpp
+++ sorting technique/insertionssort.cpp
@@ -2,8 +2,14 @@
 using namespace std;
 
 void Insertionsort(){
-    
+
 }
 int main(){
+    int arr[]={5,64,45,34,65,1,2,3,4};
+    int size=sizeof(arr)/sizeof(arr[0]);
+    cout<<"This is unsorted array-:";
+    for(int i=0;i<size;i++){
+        cout<<arr[i]<<" ";
+    }
     
 }
