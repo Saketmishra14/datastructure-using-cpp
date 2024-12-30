d065a5a06955640c05a9dc467cd276ec264eaa2f
saketmishra14 <mishrasaket042@gmail.com>
2024-12-30 22:22:57 +0530

    bubble sort

--- sorting technique/bubblesort.cpp
+++ sorting technique/bubblesort.cpp
@@ -2,6 +2,20 @@
 #include<vector>
 using namespace std;
 
+void Bubblesort(int arr[],int size){
+    for(int i=0;i<size-1;i++){
+        for(int j=0;j<size-i-1;j++){
+            if(arr[j]>arr[j+1]){
+                //swap
+                int temp=arr[j];
+                arr[j]=arr[j+1];
+                arr[j+1]=temp;
+            }
+        }
+
+    }
+}
+
 int main(){
     int arr[]={2,1,4,5,6,43,23,14};
     int size=sizeof(arr)/sizeof(arr[0]);
@@ -10,7 +24,12 @@ int main(){
     {
        cout<<arr[i]<<" ";
     }
-    
+    cout<<endl;
+    Bubblesort(arr,size);
+    cout<<"This is sorted array-:";
+    for(int i=0;i<size;i++){
+        cout<<arr[i]<<" ";
+    }
     
     
 }
