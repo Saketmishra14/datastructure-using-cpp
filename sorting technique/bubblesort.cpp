45dc70c439ca0a7d05042d2ddcc156ce84d4b742
saketmishra14 <mishrasaket042@gmail.com>
2024-12-30 22:03:44 +0530

    change

--- sorting technique/bubblesort.cpp
+++ sorting technique/bubblesort.cpp
@@ -4,5 +4,13 @@ using namespace std;
 
 int main(){
     int arr[]={2,1,4,5,6,43,23,14};
+    int size=sizeof(arr)/sizeof(arr[0]);
+    cout<<"This is unsorted array-:";
+    for (int i = 0; i <size; i++)
+    {
+       cout<<arr[i]<<" ";
+    }
+    
+    
     
 }
